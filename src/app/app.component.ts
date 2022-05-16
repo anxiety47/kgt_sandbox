9cfb030919e5e69078c22755f1c5ac8f45b8814c
Katarzyna Kalota <katarzynakalota1@gmail.com>
2022-05-16 22:40:57 +0200

    Added font awesome icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faDog, faMapMarkedAlt, faWalking, faMedal, faCalendarDay } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kgt';
+  faDog = faDog;
+  faPerson = faWalking;
+  faMap = faMapMarkedAlt;
+  faMedal = faMedal;
+  faEvent = faCalendarDay;
+
 }
